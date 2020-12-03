06fe08955b3a9d6f138091688828ebec0cab7d18
Vladperchi Won <vladperchi@gmail.com>
2020-12-03 16:20:31 -0500

    Constant Class Added For The Congifuracion

---
+++ 3.Domain/CsvImporter.Domain.Common/Constans.cs
@@ -0,0 +1,9 @@
+﻿namespace CsvImporter.Domain.Common
+{
+	public static class Constans
+	{
+		public static string CONNECTION_STRING_KEY = "connectiondDapper";
+
+		public static string DATABASE_MASTER = "master";
+	}
+}
