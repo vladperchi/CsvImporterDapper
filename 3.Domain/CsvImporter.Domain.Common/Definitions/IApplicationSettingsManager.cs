278bf217db06360eac203c63985f7596c97a8946
Vladperchi Won <vladperchi@gmail.com>
2020-12-03 16:22:53 -0500

    Added Inteface Settings Manager

---
+++ 3.Domain/CsvImporter.Domain.Common/Definitions/IApplicationSettingsManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CsvImporter.Domain.Common.Definitions
+{
+	public interface IApplicationSettingsManager
+	{
+		Task<string> GetConnectionStringValuebyKey(string key);
+	}
+}
