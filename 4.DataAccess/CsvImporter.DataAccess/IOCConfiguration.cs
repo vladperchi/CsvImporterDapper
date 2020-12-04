edf8bb0dbc8cd2984aecec5259b082315106bb7e
Vladperchi Won <vladperchi@gmail.com>
2020-12-04 10:31:51 -0500

    Added DI In Data Access

---
+++ 4.DataAccess/CsvImporter.DataAccess/IOCConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CsvImporter.DataAccess.Base;
+using CsvImporter.DataAccess.Definitions;
+using CsvImporter.DataAccess.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CsvImporter.DataAccess
+{
+	public static class IOCConfiguration
+	{
+		public static void RegisterDataAccessDependencies(this IServiceCollection container)
+		{
+			container.RegisterDataAcessBaseDependencies();
+			container.AddScoped<IStockRepository, StockRepository>();
+			container.AddScoped<IDataBaseStructureRepository, DataBaseStructureRepository>();
+		}
+	}
+}
