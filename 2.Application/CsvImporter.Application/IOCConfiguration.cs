82c86384d01d22cc7d399898ecd8eeb66f9fc9aa
Vladperchi Won <vladperchi@gmail.com>
2020-12-04 14:13:10 -0500

    Added DI Service Application

---
+++ 2.Application/CsvImporter.Application/IOCConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CsvImporter.Application.Implementation;
+using CsvImporter.DataAccess;
+using CsvImporter.Domain.Common.Definitions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CsvImporter.Application
+{
+	public static class IOCConfiguration
+	{
+		public static void RegisterApplicationDependencies(this IServiceCollection container)
+		{
+			container.AddScoped<IApplicationSettingsManager, ApplicationSettingsManager>();
+			container.RegisterDataAccessDependencies();
+			container.AddScoped<IStockProductService, StockProductService>();
+			container.AddScoped<IDataBaseStructureService, DataBaseStructureService>();
+		}
+	}
+}
